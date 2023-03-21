a848a91b895b053cc6b52b9b7c6872229d8eae67
Kris Szafranski <kris@thoughtshelter.com>
2023-03-21 15:07:35 -0500

    created gamedata tasks db

---
+++ Assets/Scripts/DataClasses/GameData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameData", menuName = "Data/GameData", order = 1)]
+public class GameData : ScriptableObject
+{
+    public TaskListData taskList;
+}
+
